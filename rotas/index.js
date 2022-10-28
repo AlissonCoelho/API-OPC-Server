9cdbc89261d48f532390bce41b8a204b3a63aea9
Alisson <alissonahc@gmail.com>
2022-10-28 20:57:11 -0300

    Leitura via HTTP usando OPCUA

--- rotas/index.js
+++ rotas/index.js
@@ -1,3 +1,14 @@
+//importar 
+const opcua = require('./opcua');
+
 module.exports = app => {
-    app.get('/teste', (req, res) => res.send('rota teste'));
+    app.get('/conectar', (req, res) => {
+        opcua.conect(req, res);
+    });
+    app.get('/desconectar', (req, res) => {
+        opcua.disconnect(req, res);
+    });
+    app.get('/lerVariavel', (req, res) => {
+        opcua.readVariable(req, res);
+    });
 }
---
+++ rotas/opcua.js
@@ -0,0 +1,57 @@
+//OPC UA
+const { OPCUAClient, AttributeIds } = require('node-opcua');
+const endpointURL = "opc.tcp://LAPTOP-HDHLV21D:49320";
+const client = OPCUAClient.create({ endpointMustExist: false });
+
+let the_session;
+class OPCUA {
+    async conect(req, res) {
+        await client.connect(endpointURL, (err) => {
+            if (err) {
+                console.log(`cannt connect to endpont: ${err}`)
+                res.status(502).send(`cannt connect to endpont: ${err}`);
+            }
+            else {
+                console.log('connected!!');
+                client.createSession((err, session) => {
+                    if (err) {
+                        console.log(`cannt create a session: ${err}`)
+                        res.status(502).send(`cannt create a session: ${err}`);
+                        return;
+                    }
+                    console.log(`seção criada`);
+                    the_session = session;
+                    res.status(200).send(`conectado e seção criada`);
+
+                })
+            }
+        })
+    };
+    async disconnect(req, res) {
+        client.closeSession(the_session, false).then(() => {
+            client.disconnect().then(() => {
+                the_session = null;
+                console.log(`desconectado`);
+                res.status(200).send(`desconectado`);
+            }).catch(err => {
+                console.log(`cannt disconnect: ${err}`);
+                res.status(502).send(`cannt disconnect: ${err}`);
+            })
+        });
+    };
+    async readVariable(req, res) {
+        if (!the_session) {
+            console.log(`desconectado`)
+            res.status(502).send(`desconectado`);
+            return;
+        }
+        await the_session.read({ nodeId: "ns=2;s=MDB.PLC1.400100", attributeId: AttributeIds.Value }, (err, dataValue) => {
+            if (!err) {
+                console.log(`value: ${dataValue.value.value}`);
+                res.status(200).send(`value: ${dataValue.value.value}`);
+            }
+        })
+    }
+}
+
+module.exports = new OPCUA();
